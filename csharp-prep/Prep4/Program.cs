b0d40e5513515c8ea45108ee3870262532dd134a
drp <dallinrp@gmail.com>
2023-01-09 13:28:06 -0700

    prep4 and prep5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers;
+        numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, end by entering 0.");
+        
+        Boolean end = false;
+        
+        while (end == false)
+        {
+        string num = Console.ReadLine();
+        int intnum = Int16.Parse(num);
+        if (intnum == 0)
+        {
+            end = true;
+        }
+        else
+        {
+        
+        numbers.Add(intnum);
+        }
+        }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum of the list is {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+        int max =  numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;   
+            }
+        }
+        Console.WriteLine($"The max is {max}");
+            }
 }
