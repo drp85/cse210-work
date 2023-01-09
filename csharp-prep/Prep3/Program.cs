f9ba8e7c37a3ca5b3f240cec170517b8e14005a9
drp <dallinrp@gmail.com>
2023-01-09 12:55:37 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int number = rnd.Next(1,100);
+        Console.WriteLine("Generated magic number");
+        bool success = false;
+        while (success == false)
+        {
+            Console.WriteLine("Guess an integer between 1 and 100.");
+            string stringguess = Console.ReadLine();
+            int guess = Int16.Parse(stringguess);
+
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                success = true;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("That's too high!");
+            }
+            else if (number > guess)
+            {
+                Console.WriteLine("That's too low!");
+            }
+            else Console.WriteLine("Error!");
+        }
     }
 }
