edf76834b242ac08de227ec5189dfc852a103033
drp <dallinrp@gmail.com>
2023-03-29 11:56:46 -0600

    Learning04 and Develop04 bones

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Mindfulness Activity Program.");
+        Console.WriteLine("\nWhat would you like to do?");
+        Console.WriteLine("\n1. ");
     }
 }
